2c5a53394d81d0447abfd249c3f194868ad93f11
Angelou <108213081+IceDoubleThree@users.noreply.github.com>
2025-06-03 15:31:06 +0800

    Change medical record table
    Changed Condition to Diagnosis

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -384,7 +384,7 @@ const AdminDashboard = () => {
                     Age
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Condition
+                    Diagnosis
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Treatment
--- client/src/pages/doctor/Dashboard.jsx
+++ client/src/pages/doctor/Dashboard.jsx
@@ -340,7 +340,7 @@ const DoctorDashboard = () => {
                     Age
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Condition
+                    Diagnosis 
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Treatment
